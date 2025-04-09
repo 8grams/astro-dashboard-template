d7dad1eb2ac2284bfacf7c142dd2f1fd55c6a243
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-09 15:32:47 +0700

    Add site name environment variable configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  env: {
+    schema: {
+      SITE_NAME: envField.string({
+        context: "client",
+        access: "public",
+        optional: false,
+      }),
+    },
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
